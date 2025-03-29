eacd0f87564cda808677e2f36b41c9db8bb44e7e
Ezekiel <iamwebwiz@gmail.com>
2025-03-29 11:41:25 +0100

    Fix db setup bugs

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from models import User, Movie
 import os
 from dotenv import load_dotenv
 
@@ -22,6 +21,9 @@ def home():
 
 # Initialize extensions
 db = SQLAlchemy(app)
+
+from models import User, Movie
+
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
-
-db = SQLAlchemy()
+from app import db
+# db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
